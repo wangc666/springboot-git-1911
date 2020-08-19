802e3f309233684569b079b3d151a6a57c5a08bb
wangc666 <1928221391@qq.com>
2020-08-19 15:34:29 +0800

    添加hello方法

--- src/main/java/com/jk/controller/UserController.java
+++ src/main/java/com/jk/controller/UserController.java
@@ -1,6 +1,13 @@
 package com.jk.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class UserController {
+
+    @RequestMapping("/hello")
+    public void hello(){
+
+    }
 
 
 }
