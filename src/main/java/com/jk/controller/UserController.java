c0442eebfaa90df14d2db8e0d19915e3c7f1ee2d
wangc666 <1928221391@qq.com>
2020-08-19 19:18:42 +0800

    å¤打个招呼

--- src/main/java/com/jk/controller/UserController.java
+++ src/main/java/com/jk/controller/UserController.java
@@ -1,9 +1,15 @@
 package com.jk.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
+
+    @RequestMapping("/hi")
+    public String hi(){
+        return "hi hahahaha";
+    }
 
 
 }
